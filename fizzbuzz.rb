4417b6a295eb25d0691022ef47d2654d2bb41f22
jadahinds <jhinds1229@pinescharter.net>
2018-07-10 20:21:36 +0000

    automatically backed up by learn

---
+++ fizzbuzz.rb
@@ -0,0 +1,16 @@
+def fizzbuzz(number)
+  if (number %3 == 0) && (number %5 == 0)
+    puts "FizzBuzz"
+  elsif number %5 == 0
+    puts "Buzz"
+  elsif number %3 == 0
+    puts "Fizz"
+  else 
+    puts "nil"
+  end
+end
+
+fizzbuzz(3)
+fizzbuzz (15)
+fizzbuzz(20)
+fizzbuzz(7)
